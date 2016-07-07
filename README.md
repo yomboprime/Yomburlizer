dac63a847731d008fa290cfcfb53eaa1e3444f96
yomboprime <yomboprime@users.noreply.github.com>
2016-07-07 08:10:19 +0200

    Initial local commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Yomburlizer
-Small node app that takes a Youtube playlist id as input and outputs a xml RSS feed file with the videos in it.
+Small node app that takes a Youtube playlist id as input and outputs a xml RSS feed file with the audios of all the videos in it, for podcasting.
+
+## Installation
+
+Just `git clone` the repository and do `node install` on the project directory.
+
+## Requirements
+
+You need the `youtube-dl` application installed and available on your path for Yomburlizer to work.
+
+Also you'll need to create a Youtube API key and write it in the `./config.json` config file.
+
+## Usage
+
+Fill in the details of your feed in the `config.json` configuration file:
+
+`apiKey`: Put your Youtube api key between the empty quotes.
+
+`playlistId`: Change this value to the id of the Youtube playlist that you want to convert to RSS feed.
+
+`thumbnail`: Size of the image thumbnails. Possible values: `"default"`, `"medium"`, `"high"`.
+
+`channelTitle`, `channelURL`, `channelDescription`: These values will transfer to your channel feed.
+
+`outputPath`: The output path of the output xml feed file, by default `"./rssFileOutput.xml"`
+
+`language`: Site language (abbreviation) (example: `"en"`)
+
+## How it works
+
+The program first fetches the playlist info and the uses youtube-dl to obtain the only-audio link for each video. As this is a long operation –the more videos, the longer–, it displays a progress bar meanwhile.
+
+Finally it produces the output RSS 2.0 xml file with the links to all the audios. It includes thumbnails and the descriptions from the video.
+
---
+++ main.js
@@ -0,0 +1,291 @@
+
+var fs = require( 'fs' );
+var spawn = require( 'child_process' ).spawn;
+var util = require( 'util' );
+var ypi = require( 'youtube-playlist-info' );
+
+console.logNoNewLine = function LogNoNewLine() {
+
+	// console.log without final "\n"
+
+	this._stdout.write( util.format.apply( this, arguments ) );
+
+};
+
+
+
+var config = loadConfig();
+
+if ( ! config ) {
+	process.exit( 1 );
+}
+
+if ( ! config.apiKey ) {
+	console.log( "Please insert your Youtube API KEY in the config.json file." );
+	process.exit( 1 );
+}
+
+console.log( "Retrieving playlist information..." );
+
+ypi.playlistInfo( config.apiKey , config.playlistId, function( playlistItems ) {
+
+	console.log( playlistItems );
+
+	console.log( "Playlist has " + playlistItems.length + " videos." );
+
+	// As the array already is ordered correctly, don't order by publish date since some videos are older than their publish date.
+	//sortItems( playlistItems );
+
+	obtainAudioURLs( playlistItems, function( errorVideos ) {
+
+		if ( errorVideos.length > 0 ) {
+			var errorInfo = "";
+			for ( var i = 0; i < errorVideos.length; i++ ) {
+				var item = playlistItems[ errorVideos[ i ].index ];
+				errorInfo += "****************************************\n";
+				errorInfo += "***** Video Id: " + item.resourceId.videoId + "\n";
+				errorInfo += "***** Error output: " + errorVideos[ i ].errorOutput + "\n";
+				errorInfo += "***** Video info below:\n";
+				errorInfo += JSON.stringify( item, null, 4 ) + "\n";
+				errorInfo += "*****\n";
+				errorInfo += "****************************************\n";
+			}
+			// Write error file
+			fs.writeFileSync( "./videoErrors.txt", errorInfo );
+			console.log( "Error: unable to get audio URL for some of the videos (" + errorVideos.length + " of total " + playlistItems.length + ")" );
+			console.log( "Info of the erroneous videos was saved on ./videoErrors.txt" );
+			console.log( "Please consider updating your youtube-dl version." );
+		}
+
+		generateOutput( playlistItems );
+
+	} );
+
+
+} );
+
+function loadConfig() {
+
+	console.log( "Loading config file in ./config.json..." );
+
+	try {
+		var configFileContent = fs.readFileSync( "./config.json", "utf-8" );
+	}
+	catch( e ) {
+		if ( e.code === 'ENOENT' ) {
+			console.error( "Error: Config file not found (path: ./config.json)" );
+		}
+		else {
+			throw e;
+		}
+	}
+
+	var config = JSON.parse( configFileContent );
+
+	if ( ! config ) {
+		console.error( "Error while loading config file in ./config.json" );
+		return null;
+	}
+
+	return config;
+
+}
+
+function obtainAudioURLs( playlistItems, onAllObtained ) {
+
+	// onAllonAllObtained( errorVideosArray )
+
+	var itemIndex = 0;
+	var numItems = playlistItems.length;
+
+	var errorVideos = [];
+
+	var barSize = 20;
+	var barMessage = "Retrieving audio URLs from the videos...";
+
+	function iterateAsync() {
+
+		if ( itemIndex < numItems ) {
+
+			var videoURL = "https://youtube.com/watch?v=" + playlistItems[ itemIndex ].resourceId.videoId;
+
+			getAudioURLFromVideo( videoURL, function( audioURL, errorOutput ) {
+
+				if ( audioURL ) {
+
+					playlistItems[ itemIndex ].resourceId.audioURL = audioURL;
+					
+				}
+				else {
+
+					playlistItems[ itemIndex ].resourceId.audioURL = null;
+
+					errorVideos.push( { index: itemIndex, errorOutput: errorOutput } );
+					
+				}
+
+				itemIndex++;
+
+				console.logNoNewLine( getProgressBarText( itemIndex, numItems, barMessage, barSize ) );
+
+				iterateAsync();
+
+			} );
+
+		}
+		else {
+
+			console.log( getProgressBarTextDelete( barMessage, barSize ) );
+
+			onAllObtained( errorVideos );
+
+		}
+
+	}
+
+	console.logNoNewLine( getProgressBarText( 0, numItems, barMessage, barSize ) );
+
+	iterateAsync();
+
+}
+
+function getAudioURLFromVideo( videoURL, onEnd ) {
+
+	// Returns a link to a media file with only the audio: onEnd( audioURL, errorOutput )
+
+	var childOutput = "";
+	var errorOutput = "";
+
+	var params = [ "--get-url", "-f", "bestaudio", videoURL ];
+
+	var childProcess = spawn( 'youtube-dl', params );
+
+	childProcess.stdout.on( 'data', function( data ) {
+
+		childOutput += data;
+
+	} );
+
+	childProcess.stderr.on( 'data', function( data ) {
+
+		errorOutput += data;
+
+	} );
+
+	childProcess.on( 'close', function( code ) {
+
+		onEnd( childOutput, errorOutput );
+
+	});
+
+}
+
+function generateOutput( playlistItems ) {
+
+	try {
+
+		console.log( "Generating output file..." );
+
+		var rssContent = "<?xml version=\"1.0\"?>\n";
+		rssContent += "<rss version=\"2.0\">\n";
+		rssContent += "<channel>\n";
+		var titleLine = "<title>" + config.channelTitle + "</title>\n";
+		var linkLine = "<link>" + config.channelURL + "</link>\n";
+		rssContent += titleLine;
+		rssContent += linkLine;
+		rssContent += "<description>" + config.channelDescription + "</description>\n";
+		rssContent += "<language>" + config.language + "</language>";
+
+		var numItems = 0;
+
+		for ( var i = 0, il = playlistItems.length; i < il; i++ ) {
+
+			var item = playlistItems[ i ];
+
+			if ( ! item.resourceId.audioURL ) {
+				continue;
+			}
+
+			rssContent += "<item>\n";
+			rssContent += "    <fecha>" + item.publishedAt + "</fecha>\n";
+			rssContent += "    <title>" + item.title + "</title>\n";
+			rssContent += "    <link>" + item.resourceId.audioURL + "</link>\n";
+			rssContent += "    <description>" + item.description + "</description>\n";
+			rssContent += "    <image>\n";
+			rssContent += "        <url>" + item.thumbnails[ config.thumbnail ].url + "</url>\n";
+			rssContent += "        " + titleLine;
+			rssContent += "        " + linkLine;
+			rssContent += "    </image>\n";
+			rssContent += "</item>\n";
+
+			numItems++;
+		}
+
+		rssContent += "</channel>\n";
+
+		// write output file
+		fs.writeFileSync( config.outputPath, rssContent );
+
+		console.log( "Finished writing RSS feed file: " + numItems + " items written.\nHave a good day." );
+
+	}
+	catch ( err ) {
+		console.log( "Some error occured while processing the data: " + err );
+	}
+
+}
+
+function getProgressBarText( progress, maxProgress, message, barSize ) {
+
+	var line = "\r" + message + "[";
+
+	var fraction = progress / maxProgress;
+
+	var numProgressBars = Math.floor( fraction * barSize );
+
+	for ( var i = 0; i < numProgressBars; i++ ) {
+		line += "=";
+	}
+	for ( var i = numProgressBars; i < barSize; i++ ) {
+		line += " ";
+	}
+
+	var percent = Math.floor( fraction * 100 );
+
+	line += "] " + percent + "%";
+
+	return line;
+}
+
+function getProgressBarTextDelete( message, barSize ) {
+
+	var line = "";
+	var n = message.length + barSize + 7;
+	for ( var i = 0; i < n; i++ ) {
+		line += " ";
+	}
+	return line;
+}
+
+function sortItems( playlistItems ) {
+
+	console.log( "Sorting items..." );
+
+	playlistItems.sort( function( a , b ) {
+
+		if ( a.publishedAt < b.publishedAt ) {
+
+			return 1;
+
+		}
+		else if ( a.publishedAt > b.publishedAt ) {
+
+			return -1;
+
+		}
+
+		return 0;
+
+	} );
+
+}
